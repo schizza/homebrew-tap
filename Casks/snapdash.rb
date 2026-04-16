95d7836b075c5feff87fc22305cf8dc3ea467759
Lukas Svoboda <opensource@schizza.cz>
2026-04-16 10:44:43 +0200

    Fix sha256 assignment and update zap paths

--- Casks/snapdash.rb
+++ Casks/snapdash.rb
@@ -2,7 +2,7 @@ cask "snapdash" do
   version "0.0.1"
 
   on_arm do
-    sha256 = "08ba098d2f8699be06afd944d4ab6a556c6821f5d5ff23a678aa7988e0d45364"
+    sha256 "08ba098d2f8699be06afd944d4ab6a556c6821f5d5ff23a678aa7988e0d45364"
     url "https://github.com/schizza/snapdash/releases/download/v#{version}/snapdash-v#{version}-macos-aarch64.tar.gz"
   end
 
@@ -15,8 +15,8 @@ cask "snapdash" do
   app "Snapdash.app"
 
   zap trash: [
-    "~Library/Application Support/dev.snapdash.Snapdash",
-    "~Library/Preferences/dev.snapdash.Snapdash.plist",
-    "~Library/Caches/dev.snapdash.Snapdast",
+    "~/Library/Application Support/dev.snapdash.Snapdash",
+    "~/Library/Preferences/dev.snapdash.Snapdash.plist",
+    "~/Library/Caches/dev.snapdash.Snapdast",
   ]
 end
