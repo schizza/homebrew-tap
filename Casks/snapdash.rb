ccc705eae393ad23e2f47708e5bde3ca53746fe5
SchiZzA <opensource@schizza.cz>
2026-04-16 09:26:48 +0200

    Add snapdash cask (v0.0.1)

--- Casks/snapdash.rb
+++ Casks/snapdash.rb
@@ -10,7 +10,7 @@ cask "snapdash" do
   desc "Pluggable desktop widget system for live data sources"
   homepage "https://snapdash.schizza.cz"
 
-  dependes_on macos: ">= :big_sur"
+  depends_on macos: ">= :big_sur"
 
   app "Snapdash.app"
 
