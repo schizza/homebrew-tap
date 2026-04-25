51cb0de5957f334bf7d99728c7240be61a862748
Lukas Svoboda <opensource@schizza.cz>
2026-04-25 11:13:34 +0200

    Update snapdash version to 0.0.3

--- Casks/snapdash.rb
+++ Casks/snapdash.rb
@@ -1,8 +1,8 @@
 cask "snapdash" do
-  version "0.0.2"
+  version "0.0.3"
 
   on_arm do
-    sha256 "1d6b706a7b0369998fefa2475753f01c5ec51650c8a1327b35eb88016810aa01"
+    sha256 "55d5d06a07e7bda8c908bdfc720a4cd118d0b1e05ad0f062aa79fa4e03e7461d"
     url "https://github.com/schizza/snapdash/releases/download/v#{version}/snapdash-v#{version}-macos-aarch64.tar.gz",
       verified: "github.com/schizza/snapdash/"
   end
